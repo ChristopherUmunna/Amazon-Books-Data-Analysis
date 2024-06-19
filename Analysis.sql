9e13db4030a8e7468359f902382b8bc5888b9827
christoumunna <christoumunna01@gmail.com>
2024-06-19 22:03:52 +0100

    upload files

---
+++ Analysis.sql
@@ -0,0 +1,136 @@
+-- THE DATA USED FOR THIS QUERY IS FROM KAGGLES AND IT CONTAINS THE TOP 5O BEST SELLING BOOKS FROM 2009-2020
+
+
+-- 1) Author with the highest number of best selling books?
+SELECT author,COUNT(name) as 'Number of Best Selling Books'
+FROM amazon_bestsellers
+GROUP BY author
+ORDER BY count(name) desc
+LIMIT 1;
+
+-- Jeff Kinney has the highest with 13 books
+
+
+-- 2) The Average price of Jeff Kinney books
+SELECT ROUND(AVG(price), 2)
+FROM amazon_bestsellers
+WHERE author = "Jeff Kinney"
+
+-- Jeff Kinney books cost an average of $9.08
+
+
+-- 3) Most Expensive Best selling Book on Amazon?
+SELECT *
+FROM amazon_bestsellers
+ORDER BY price DESC
+LIMIT 1;
+
+-- The Most Expensive Best selling Book is "Diagnostic and Statistical Manual of Mental Disorders, 5th Edition: DSM-5" which costs $105,
+-- it has a 4.5 rating and was released in 2013
+
+
+-- 4) Average price of the best selling books?
+SELECT AVG(price) as Average_price
+FROM amazon_bestsellers
+
+-- The Average price of the books is $12.92
+
+
+-- 5) What genre sold the most?
+SELECT genre, COUNT(genre) as count
+FROM amazon_bestsellers
+GROUP BY genre
+
+-- There are 337 Non-fiction books and 263 Fiction books
+
+
+-- 6) Average price of both genre?
+SELECT genre, ROUND(AVG(price), 1) as Average_Price
+FROM amazon_bestsellers
+GROUP BY genre
+
+-- Non-Fiction is $14.7 and Fiction is 10.6 dollars
+
+
+-- 7) Overall Best Selling book?
+SELECT name, COUNT(name) as number
+FROM amazon_bestsellers
+GROUP BY name
+ORDER BY COUNT(name) DESC
+LIMIT 1;
+
+-- The Overall Best Selling Book is "Publication Manual of the American Psychological Association, 6th Edition"
+
+
+-- 8) Top Rated Book
+SELECT name, user_rating
+FROM amazon_bestsellers
+ORDER BY user_rating DESC
+
+-- There are 61 books with 4.9 rating
+
+
+-- 9)Least Rated Book
+SELECT name, user_rating
+FROM amazon_bestsellers
+ORDER BY user_rating 
+LIMIT 1
+-- "The Casual Vacancy" is the least rated with a rating of 3.3
+
+
+-- 10) Average User Ratings of the best sellers
+SELECT ROUND(AVG(user_rating),2)
+FROM amazon_bestsellers
+
+-- The average rating of all the books is 4.63
+
+
+-- 11) The Book with the highest number of reviews
+SELECT *
+FROM amazon_bestsellers
+ORDER BY reviews desc
+LIMIT 1
+
+-- "Where the Crawdads Sing" has the highest with 120727 reviews
+
+
+-- 12) The Rating of the book with the highest number of reviews
+SELECT name, reviews, user_rating
+FROM amazon_bestsellers
+WHERE reviews = 120727
+
+-- The book has a user rating of 4.8
+
+
+
+-- 13) The Book with the lowest number of reviews
+SELECT *
+FROM amazon_bestsellers
+ORDER BY reviews
+LIMIT 1
+
+-- "Divine Soul Mind Body Healing and Transmission System" has the lowest with 37 reviews
+
+
+-- 14) The Rating of the book with the lowest number of reviews
+SELECT name, reviews, user_rating
+FROM amazon_bestsellers
+WHERE reviews = 37
+
+-- The book has a user rating of 4.6
+
+
+-- 15) The Average number of Reviews
+SELECT AVG(reviews)
+FROM amazon_bestsellers
+
+-- The average number of reviews is 13941.73
+
+
+-- 16) The total mumber of reviws in each year?
+SELECT year,SUM(reviews) as total_number_of_reviews
+FROM amazon_bestsellers
+GROUP BY year
+ORDER BY SUM(reviews) DESC
+
+-- 2020 has the most reviews(1790733), followed by 2019(794917) while 2009 has the least (235506)
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Amazon-Books-Data-Analysis
-This is the data analysis of top 50 Amazon Book for each year from 2009 to 2020
+# Data Analysis of the Amazon Top 50 Bestselling Books 2009-2020 Dataset.
+
+## Objective
+Extensively analyze the data using SQL
+
+## Data
+The dataset is a table file of the Top 50 Bestselling Books between 2009 and 2020 on Amazon. It was gotten from [Kaggle](https://www.kaggle.com/palanjali007/amazons-top-50-bestselling-novels-20092020). It contains:
+* Name
+* Author
+* User_Rating
+* Reviews
+* Price
+* Year
+* Genre
+
+## [Data Cleaning](https://github.com/VictorUmunna/SQL-Data-Analysis-on-Amazon-Top-50-Best-selling-Books-2009-2020-Dataset./blob/master/data_cleaning.ipynb)
+The whitepaces in the name of some authors were removed inorder for them to match correctly. The cleaned [dataset](https://github.com/VictorUmunna/SQL-Data-Analysis-on-Amazon-Top-50-Best-selling-Books-2009-2020-Dataset./blob/master/amazon_bestsellers.csv) is then analyzed
+
+## [SQL Analysis](https://github.com/VictorUmunna/SQL-Data-Analysis-on-Amazon-Top-50-Best-selling-Books-2009-2020-Dataset./blob/master/Analysis.sql)
+This analysis was made to derive insights from the dataset.
+
+## [Tableau Dashboard](https://public.tableau.com/app/profile/victor.umunna/viz/AmazonBestsellers_16431497744210/Dashboard1)
+![Dashboard](https://user-images.githubusercontent.com/58162911/151429443-947be556-89aa-496d-b4c4-59d243fb99da.png)
